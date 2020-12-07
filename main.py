33e4473a01dcc78b1ccc59b29276cae661c82ad2
josue-avila <josue.avila@olist.com>
2020-12-07 15:53:09 -0300

    adding class

--- main.py
+++ main.py
@@ -1,108 +1,110 @@
 
-name: str
-description: str
-price: float
+class Product:
+    name: str
+    description: str
+    price: float
 
-list_names: list = []
-list_description: list = []
-list_price: list = []
+    list_names: list = []
+    list_description: list = []
+    list_price: list = []
 
-def menu():
-    print("MENU:")
-    print('1 - Adicionar novo produto')
-    print('2 - Ver produtos cadastrados')
-    print('3 - Editar produto')
-    print('4 - Deletar produto')
-    print('5 - Sair')
-    print('')
-    option = input('Digite a opção escolhida (use os números): -> ')
-    return option
-
-def add():
-    name = input("Digite o nome do novo produto: ")
-
-    if name in list_names:
+    def menu(self):
+        print("MENU:")
+        print('1 - Adicionar novo produto')
+        print('2 - Ver produtos cadastrados')
+        print('3 - Editar produto')
+        print('4 - Deletar produto')
+        print('5 - Sair')
         print('')
-        print('O PRODUTO JÁ EXISTE!')
+        self.option = input('Digite a opção escolhida (use os números): -> ')
+        return self.option
+
+    def add(self):
+        name = input("Digite o nome do novo produto: ")
+
+        if name in self.list_names:
+            print('')
+            print('O PRODUTO JÁ EXISTE!')
+            print('')
+            pass
+            
+        else:
+            self.list_names.append(name)
+            description = input("Inclua uma breve descrição: ")
+            self.list_description.append(description)
+            price = input("Adicione o preço: ")
+            self.list_price.append(price)
+            print('')
+            print('PRODUTO ADICIONADO!')
+            print('')
+            pass
+
+
+    def get_products(self): 
+        print('LISTA DE PRODUTOS: ', self.list_names) 
+        pass 
+
+    def delete(self):
         print('')
-        pass
+        element = input('Digite o nome do produto que deseja excluir: ')
+        if element in self.list_names:
+            index = self.list_names.index(element)
+            self.list_names.pop(index)
+            self.list_description.pop(index)
+            self.list_price.pop(index)
+            print('')
+            print('ITEM EXCLUIDO!')
+            print('')
+            pass
+        else:
+            print('')
+            print('PRODUTO NÃO CADASTRADO!')
+            print('')
+
+    def edit_products(self):
+        print('')
+        element = input('Digite o nome do produto que deseja editar: ')
+        if element in self.list_names:
+            index = self.list_names.index(element)
         
-    else:
-        list_names.append(name)
-        description = input("Inclua uma breve descrição: ")
-        list_description.append(description)
-        price = input("Adicione o preço: ")
-        list_price.append(price)
-        print('')
-        print('PRODUTO ADICIONADO!')
-        print('')
-        pass
+            name = input('Digite o novo nome do produto: ')
+            if name == '':
+                pass
+            else:
+                self.list_names[index] = name
+                pass
 
+            description = input('Digite a nova descrição do produto: ')
+            if description == '':
+                pass
+            else:
+                self.list_description[index] = description
+                pass
 
-def get_products(): 
-    print('LISTA DE PRODUTOS: ', list_names) 
-    pass 
-
-def delete():
-    print('')
-    element = input('Digite o nome do produto que deseja excluir: ')
-    if element in list_names:
-        index = list_names.index(element)
-        list_names.pop(index)
-        list_description.pop(index)
-        list_price.pop(index)
-        print('')
-        print('ITEM EXCLUIDO!')
-        print('')
-        pass
-    else:
-        print('')
-        print('PRODUTO NÃO CADASTRADO!')
-        print('')
-
-def edit_products():
-    print('')
-    element = input('Digite o nome do produto que deseja editar: ')
-    if element in list_names:
-        index = list_names.index(element)
-    
-        name = input('Digite o novo nome do produto: ')
-        if name == '':
-            pass
+            price = input('Digite o novo preço do produto: ')
+            if price == '':
+                pass
+            else:
+                self.list_price[index] = price
+                pass
         else:
-            list_names[index] = name
-            pass
+            print('')
+            print('PRODUTO NÃO CADASTRADO!')
+            print('')
 
-        description = input('Digite a nova descrição do produto: ')
-        if description == '':
-            pass
-        else:
-            list_description[index] = description
-            pass
+product = Product()
 
-        price = input('Digite o novo preço do produto: ')
-        if price == '':
-            pass
-        else:
-            list_price[index] = price
-            pass
-    else:
-        print('')
-        print('PRODUTO NÃO CADASTRADO!')
-        print('')
-
-
-opcao = menu()
+opcao = product.menu()
 while opcao != '5':
     if opcao == '1':
-        add()
+        product.add()
     elif opcao == '2':
-        get_products()
+        product.get_products()
     elif opcao == '3':
-        edit_products()
+        product.edit_products()
     elif opcao == '4':
-        delete()
+        product.delete()
     
 
-    opcao = menu()
+    opcao = product.menu()
     
